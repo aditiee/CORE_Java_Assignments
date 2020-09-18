d8280dc6fe7a9a3f0a6d0a6fa6bc3218eeacbf91
aditiee <65465599+aditiee@users.noreply.github.com>
2020-09-18 23:39:30 +0530

    Update Eligible.java

--- DAY01/Eligible.java
+++ DAY01/Eligible.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
-public class Eligible{
+public class Eligible
+{
 
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.print(" PERSON'S GENDER MALE(m) OR FEMALE(f): ");
-		char gender = sc.next.charAt(0);
+		char gender = sc.next().charAt(0);
 		System.out.print(" PERSON'S AGE :");
-		int age = sc.nextInt();
-		if( ch == 'm' && age >=21){
-				System.out.println("Person is Eligible for Marriage");
-			}
-			else if ( ch == 'f' && age >=18){
-				System.out.println("Person is Eligible for Marriage");
-			}
-			else{	
-				System.out.println("Person is not Eligible for Marriage");
-			}
+		int Age = sc.nextInt();
+		if( gender == 'm' && Age >=21)
+		{
+			System.out.println("Person is Eligible for Marriage");
+		}
+		else if ( gender == 'f' && Age >=18)
+		{
+			System.out.println("Person is Eligible for Marriage");
+		}
+		else
+		{	
+			System.out.println("Person is not Eligible for Marriage");
+		}
 	}
-
-
-
-
 
 }
